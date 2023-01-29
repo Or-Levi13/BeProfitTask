5b129f0f109423bd1b20777e85ffa5e7d76ef98e
Or Levi <levior9@gmail.com>
2023-01-29 16:34:05 +0200

    init vuetify

---
+++ BeProfitTask/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <Header />
+  <DataTable />
+</template>
+
+<script lang="ts" setup>
+import Header from '@/components/Header.vue'
+import DataTable from '@/components/DataTable.vue'
+</script>
